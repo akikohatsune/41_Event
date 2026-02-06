e1fdcd7d15cb891bb107eaf670340f8bf88db391
HyperText Miku Language! <k3212700@gmail.com>
2026-02-06 07:31:08 +0700

    Add files via upload

---
+++ Check41.bat
@@ -0,0 +1,3 @@
+cd /d %~dp0
+powershell -NoProfile -ExecutionPolicy bypass -file %~p0\Check41.ps1
+pause
---
+++ Check41.ps1
@@ -0,0 +1,29 @@
+
+pushd $PSScriptRoot
+$result = $true
+$errmessage = ""
+$eventId = 41
+$logName = "System"
+$events = Get-WinEvent -LogName $logName | Where-Object { $_.Id -eq $eventId }
+
+if ($events) {
+    foreach ($event in $events) {
+        $result = $result -and $false
+        Write-Host "Event ID: $($event.Id)"
+        Write-Host "Time Created: $($event.TimeCreated)"
+        Write-Host "Message: $($event.Message)"
+    }
+} 
+else {
+    Write-Host 'No events with ID $eventId found in the $logName log.'
+}
+
+
+#Check if there's eventlog about going to CS mode
+if($result){
+    Write-Output "No 41 events"
+    cmd.exe /c color 27
+}
+else{
+    cmd.exe /c color 47
+}
